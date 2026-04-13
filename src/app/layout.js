858f45cd0e7061d7b987a4085727e16182669a22
sohanbd413-prog <sohanbd413@gmail.com>
2026-04-14 00:31:31 +0600

    footer complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/homepage/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,10 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar></Navbar>
-        {children}</body>
+        {children}
+        <Footer></Footer>
+        </body>
+        
     </html>
   );
 }
